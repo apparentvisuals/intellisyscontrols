803f0697d7a2ff2411ebee9b73e9ab7c3f4ee05a
Bailey Hu <3787826+apparentvisuals@users.noreply.github.com>
2019-09-14 21:44:36 -0400

    migrate intellisys from ipage

---
+++ .vuepress/config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  title: "Intellisys Controls",
+  description:
+    "Industrial wireless controllers - Builtin quality at every step",
+  themeConfig: {
+    search: false,
+    nav: [
+      {
+        text: "Home",
+        link: "/"
+      },
+      {
+        text: "Products",
+        items: [
+          {
+            text: "Connectivity",
+            items: [
+              {
+                text: "DT1000",
+                link: "/products/dt1000"
+              },
+              {
+                text: "EMB2000",
+                link: "/products/emb2000"
+              }
+            ]
+          },
+          {
+            text: "OEM",
+            items: [
+              {
+                text: "Flex2400 Tranceiver",
+                link: "/products/flex2400"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        text: 'About',
+        link: '/about'
+      },
+      {
+        text: 'Contact us',
+        link: '/contact-us'
+      }
+    ]
+  }
+  // postcss: {
+  //   plugins: [require('tailwindcss')('./tailwind.js'), require('autoprefixer')],
+  // },
+};
---
+++ README.md
@@ -0,0 +1,21 @@
+---
+home: true
+heroImage: /img/intellisys-controls.png
+heroText: Intellisys Controls
+tagline: Built-in quality at every step
+---
+
+<div class="features">
+  <div class="feature" style="text-align: center">
+    <a href="/products/flex2400"><img src="/img/flex2400.png"></a>
+    <h2>OEM Integration</h2>
+  </div>
+  <div class="feature" style="text-align: center">
+    <img src="/img/rbx0001.png">
+    <h2>Remote Control</h2>
+  </div>
+  <div class="feature" style="text-align: center">
+    <img src="/img/bux0001.jpg">
+    <h2>Installations</h2>
+  </div>
+</div>
